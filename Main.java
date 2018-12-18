475826236203d028b83fc3e1135defa22ff32eac
nilkkapaineet <logintili@gmail.com>
2018-12-19 00:51:43 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 package com.company;
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,69 +27,93 @@ public class Main {
             BufferedReader bufferedReader =
                     new BufferedReader(fileReader);
 
-            int maxHeight = 0;
-            int maxWidth = 0;
-            List<Integer[]> lines = new ArrayList<Integer[]>();
+            int howManyLines = 0;
+            int lineLength = 0;
+            List<String> lines = new ArrayList<>();
             while((line = bufferedReader.readLine()) != null) {
-                Pattern p = Pattern.compile("[0-9]+");
-                Matcher m = p.matcher(line);
-                Integer[] ns = new Integer[5];
-                int i = 0;
-                while (m.find()) {
-                    int n = Integer.parseInt(m.group());
-                    // append n to list
-                    ns[i] = n;
-                    i++;
-                }
-                lines.add(ns);
-
-                if (ns[1]+ns[3]+1 > maxWidth) {
-                    maxWidth = ns[1]+ns[3]+1;
-                }
-                if (ns[2]+ns[4]+1 > maxHeight) {
-                    maxHeight = ns[2]+ns[4]+1;
-                }
-
-  //              System.out.println(ns[0] + " " + ns[1] + " " + ns[2] + " " + ns[3] + " " + ns[4]);
+                howManyLines++;
+                lines.add(line);
+                lineLength = line.length();
+   //             char first = line.charAt(5);
             }
             // Always close files.
             bufferedReader.close();
 
-//            System.out.println(maxHeight + " " + maxWidth + " " + lines.size() );
-
-            int[][] grid = new int[maxWidth][maxHeight];
-            for (int i=0; i<maxWidth; i++) {
-               for (int j=0; j<maxHeight; j++) {
-                   grid[i][j] = 0;
-               }
+            char[][] grid = new char[lineLength][howManyLines];
+            for (int i=0; i<lines.size(); i++) {
+                String tempLine = lines.get(i);
+                // read initial state, there's either . # or |  as a state
+                for (int j=0; j<lineLength; j++) {
+                    grid[j][i] = tempLine.charAt(j);
+                }
             }
 
-            // fill grid
-            int numberOfMultipleClaims = 0;
-            for (int i=0; i<lines.size(); i++) {
-                // (1,3) 4*4
-                // x,y,w,h
-                Integer[] n = lines.get(i);
-                for (int x=n[1]; x<(n[1]+n[3]); x++) {
-                    for (int y=n[2]; y<(n[2]+n[4]); y++) {
-                        grid[x][y] += 1;
-                        if (grid[x][y] == 2) {
-                            numberOfMultipleClaims += 1;
+            // grid initialized, go through a process but first we need a copy of the grid
+            char[][] gridCopy = new char[lineLength][howManyLines];
+            for (int i=0; i<howManyLines; i++) {
+                // read initial state, there's either . # or |  as a state
+                for (int j=0; j<lineLength; j++) {
+                    gridCopy[j][i] = grid[j][i];
+                }
+            }
+            //System.arraycopy( grid, 0, gridCopy, 0, grid.length );
+
+
+            for (int i=0; i<howManyLines; i++) {
+                // read initial state, there's either . # or |  as a state
+                for (int j=0; j<lineLength; j++) {
+                    System.out.print(grid[j][i]);
+                }
+                System.out.println("");
+            }
+            System.out.println("----------initial-----------------------");
+
+            // open "." acre will become filled with trees if three or more adjacent acres contained trees. Otherwise, nothing happens.
+            // An acre filled with trees "|" will become a lumberyard if three or more adjacent acres were lumberyards. Otherwise, nothing happens.
+            // An acre containing a lumberyard "#" will remain a lumberyard if it was adjacent to at least one other lumberyard and at least one acre containing trees. Otherwise, it becomes open.
+            // there are max 8 adjacent acres
+
+            for (int rounds=0; rounds<10; rounds++) {
+                for (int i = 0; i < howManyLines; i++) { // y
+                    for (int j = 0; j < lineLength; j++) { // x
+                        char c = grid[j][i];
+                        if (c == '.') {
+                            gridCopy[j][i] = processOpenGround(j, i, c, grid, howManyLines, lineLength);
                         }
+                        if (c == '|') {
+                            gridCopy[j][i] = processTrees(j, i, c, grid, howManyLines, lineLength);
+                        }
+                        if (c == '#') {
+                            gridCopy[j][i] = processLumberyard(j, i, c, grid, howManyLines, lineLength);
+                        }
+                    }
+                }
+                // next state is in the gridCopy
+                for (int i = 0; i < howManyLines; i++) {
+                    // read initial state, there's either . # or |  as a state
+                    for (int j = 0; j < lineLength; j++) {
+                        grid[j][i] = gridCopy[j][i];
                     }
                 }
             }
 
-            System.out.println("Number of multiple claims: " + numberOfMultipleClaims);
-
-            /*
-            for (int i=0; i<maxWidth; i++) {
-                for (int j=0; j<maxHeight; j++) {
-                    System.out.print(grid[i][j]);
+            // get score
+            int wooden = 0;
+            int ly = 0;
+            for (int i=0; i<howManyLines; i++) {
+                // read initial state, there's either . # or |  as a state
+                for (int j=0; j<lineLength; j++) {
+                    System.out.print(gridCopy[j][i]);
+                    if (gridCopy[j][i] == '|') {
+                        wooden++;
+                    }
+                    if (gridCopy[j][i] == '#') {
+                        ly++;
+                    }
                 }
-                System.out.println();
+                System.out.println("");
             }
-            */
+            System.out.println("score: " + wooden + "*" + ly + "= " + (wooden*ly) );
 
         }
 
@@ -103,4 +130,75 @@ public class Main {
             // ex.printStackTrace();
         }
     }
+
+    public static char processOpenGround(int x, int y, char input, char[][] grid, int howManyLines, int lineLength) {
+        // check adjacent acres i.e. +/- 1 node unless over the limits
+        // if three or more trees --> |, otherwise .
+        int trees = 0;
+        for (int yy = -1; yy <= 1; yy++) {
+            for (int xx = -1; xx <= 1; xx++) {
+                if ((xx+x) == x && (yy+y) == y) {
+                    continue; // You are not neighbor to yourself
+                }
+                if ((xx+x) >= 0 && (yy+y) >= 0 && (xx+x) < lineLength && (yy+y) < howManyLines) {
+                    if (grid[xx+x][yy+y] == '|') {
+                        trees++;
+                    }
+                }
+            }
+        }
+
+        if (trees >= 3) {
+            return '|';
+        } else {
+            return '.';
+        }
+    }
+
+    public static char processTrees(int x, int y, char input, char[][] grid, int howManyLines, int lineLength) {
+        int lumberyards = 0;
+        for (int yy = -1; yy <= 1; yy++) {
+            for (int xx = -1; xx <= 1; xx++) {
+                if ((xx+x) == x && (yy+y) == y) {
+                    continue; // You are not neighbor to yourself
+                }
+                if ((xx+x) >= 0 && (yy+y) >= 0 && (xx+x) < lineLength && (yy+y) < howManyLines) {
+                    if (grid[xx+x][yy+y] == '#') {
+                        lumberyards++;
+                    }
+                }
+            }
+        }
+        if (lumberyards >= 3) {
+            return '#';
+        } else {
+            return '|';
+        }
+    }
+
+    public static char processLumberyard(int x, int y, char input, char[][] grid, int howManyLines, int lineLength) {
+        int lumberyards = 0;
+        int trees = 0;
+        for (int yy = -1; yy <= 1; yy++) {
+            for (int xx = -1; xx <= 1; xx++) {
+                if ((xx+x) == x && (yy+y) == y) {
+                    continue; // You are not neighbor to yourself
+                }
+                if ((xx+x) >= 0 && (yy+y) >= 0 && (xx+x) < lineLength && (yy+y) < howManyLines) {
+                    if (grid[xx+x][yy+y] == '|') {
+                        trees++;
+                    }
+                    if (grid[xx+x][yy+y] == '#') {
+                        lumberyards++;
+                    }
+                }
+            }
+        }
+        if (lumberyards >= 1 && trees >= 1) {
+            return '#';
+        } else {
+            return '.';
+        }
+    }
+
 }
